83a2aac6a5359133b725e5eb98ec2d73e64bbbfa
Efren Galarza <efren.galarza@gizlocorp.com>
2023-06-15 15:04:56 -0500

    feat(repository): Añadir funcionalidad de búsqueda por número de identificación o nombre

--- src/main/java/com/alquimiasoft/minegocio/repository/IClientRepository.java
+++ src/main/java/com/alquimiasoft/minegocio/repository/IClientRepository.java
@@ -2,8 +2,14 @@ package com.alquimiasoft.minegocio.repository;
 
 import com.alquimiasoft.minegocio.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IClientRepository extends JpaRepository<ClientEntity, UUID> {
+    @Query(value = "SELECT * FROM client_entity WHERE identificationNumber = :identificationNumber OR CONCAT(firstName, lastName) = :name", nativeQuery = true)
+    List<ClientEntity> findAllByIdentificationNumberOrName(@Param("identificationNumber") String identificationNumber, @Param("name") String name);
+
 }
